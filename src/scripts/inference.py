6b24ef937f83c31184c6c24d57d13661295ec26c
patrick <742679653@qq.com>
2025-09-12 23:28:38 +0800

    fix: add pytorch grad cam dependencies

--- src/scripts/inference.py
+++ src/scripts/inference.py
@@ -10,5 +10,17 @@ from models.vision_transformer import ViT
 from data.common_datasets import get_mnist_train_loader, get_cifar10_datamodule
 from pprint import pprint
 from tqdm import tqdm
+from pytorch_grad_cam import (
+    GradCAM,
+    HiResCAM,
+    ScoreCAM,
+    GradCAMPlusPlus,
+    AblationCAM,
+    XGradCAM,
+    EigenCAM,
+    FullGrad,
+)
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image
 import yaml
 import os
