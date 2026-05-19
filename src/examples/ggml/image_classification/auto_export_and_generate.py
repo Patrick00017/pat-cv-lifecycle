c3c31f3ec016929ccd744f00c93cc94a6ceb4739
patrick <742679653@qq.com>
2026-05-19 21:28:36 +0800

    example: create startup template

--- src/examples/ggml/image_classification/auto_export_and_generate.py
+++ src/examples/ggml/image_classification/auto_export_and_generate.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 from torch.export import export, ExportedProgram
 from torch.export.graph_signature import InputSpec, OutputSpec
 from torchvision import datasets, transforms, models
+from string import Template
+
+aten_op_dict = []
 
 
 class AutoGenerateUtils:
@@ -13,6 +16,95 @@ class AutoGenerateUtils:
         self.graph = self.exported_program.graph
         self.input_specs: list[InputSpec] = self.graph_signature.input_specs
         self.output_specs: list[OutputSpec] = self.graph_signature.output_specs
+        self.ggml_code: str = ""
+        self.init_ggml_code()
+
+    def init_ggml_code(self):
+        """this function is used to generate some useful tools for next generation."""
+        ggml_code_startup_template_str = """
+#define _USE_MATH_DEFINES // for M_PI
+#define _CRT_SECURE_NO_DEPRECATE // Disables ridiculous "unsafe" warnigns on Windows
+
+#include "ggml.h"
+#include "ggml-cpu.h"
+#include "ggml-alloc.h"
+#include "ggml-backend.h"
+#include "gguf.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#include <cassert>
+#include <cmath>
+#include <cstddef>
+#include <cstdio>
+#include <cstring>
+#include <fstream>
+#include <map>
+#include <string>
+#include <vector>
+#include <thread>
+#include <cinttypes>
+#include <iostream>
+
+
+#if defined(_MSC_VER)
+#pragma warning(disable: 4244 4267) // possible loss of data
+#endif
+
+${model_struct}
+
+bool model_init_from_file(const std::string &fname, custom_model &model){
+    struct ggml_context * tmp_ctx = nullptr;
+    struct gguf_init_params gguf_params = {
+        /*.no_alloc   =*/ false,
+        /*.ctx        =*/ &tmp_ctx,
+    };
+    gguf_context * gguf_ctx = gguf_init_from_file(fname.c_str(), gguf_params);
+    if (!gguf_ctx) {
+        fprintf(stderr, "%s: gguf_init_from_file() failed\n", __func__);
+        return false;
+    }
+
+    int num_tensors = gguf_get_n_tensors(gguf_ctx);
+    struct ggml_init_params params {
+            /*.mem_size   =*/ ggml_tensor_overhead() * num_tensors,
+            /*.mem_buffer =*/ NULL,
+            /*.no_alloc   =*/ true,
+    };
+    model.ctx = ggml_init(params);
+    for (int i = 0; i < num_tensors; i++) {
+        const char * name = gguf_get_tensor_name(gguf_ctx, i);
+        struct ggml_tensor * src = ggml_get_tensor(tmp_ctx, name);
+        struct ggml_tensor * dst = ggml_dup_tensor(model.ctx, src);
+        ggml_set_name(dst, name);
+
+        // add some log
+        std::cout << "src: " << src->name << " dst: " << dst->name << std::endl;
+    }
+    model.buffer = ggml_backend_alloc_ctx_tensors(model.ctx, model.backend);
+    // copy tensors from main memory to backend
+    for (struct ggml_tensor * cur = ggml_get_first_tensor(model.ctx); cur != NULL; cur = ggml_get_next_tensor(model.ctx, cur)) {
+        struct ggml_tensor * src = ggml_get_tensor(tmp_ctx, ggml_get_name(cur));
+        size_t n_size = ggml_nbytes(src);
+        ggml_backend_tensor_set(cur, ggml_get_data(src), 0, n_size);
+    }
+    gguf_free(gguf_ctx);
+    ggml_free(tmp_ctx);
+    
+    // load model.ctx tensors into backbone
+    ${weight_load_process}
+    
+    return true;
+}
+        """
+        ggml_startup_template = Template(ggml_code_startup_template_str)
+        print(
+            ggml_startup_template.substitute(
+                model_struct="aaa\n", weight_load_process="bbb\n"
+            )
+        )
 
     def check_input(self):
         for i in range(5):
@@ -43,8 +135,7 @@ Node:
     type: {node.type},
     input_nodes: {node.all_input_nodes},
     args: {node.args}
-    kwargs: {node.kwargs},
-    meta: {node.meta}
+    kwargs: {node.kwargs}
 """)
 
     def generate(self):
@@ -56,4 +147,4 @@ if __name__ == "__main__":
     input_args = (torch.randn(1, 3, 256, 256),)
     tool = AutoGenerateUtils(model=model, input_args=input_args)
     # tool.check_input()
-    tool.check_graph()
+    # tool.check_graph()
